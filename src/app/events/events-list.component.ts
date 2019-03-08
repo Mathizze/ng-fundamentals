7fcc596746e0f7cc4c5a0337925feda94b9b6b94
mathijs.hardeman <mathijs.hardeman@MC-C2VVBG2>
2019-03-08 14:02:44 +0100

    lazy loadling

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from './shared/event.service';
-import { toBase64String } from '@angular/compiler/src/output/source_map';
 import { ToastrService } from '../common/toastr.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   template: `
@@ -19,12 +19,12 @@ import { ToastrService } from '../common/toastr.service';
 export class EventsListComponent implements OnInit {
   events: any[];
 
-  constructor(private eventService: EventService, private toastr: ToastrService) {
+  constructor(private eventService: EventService, private toastr: ToastrService, private route:ActivatedRoute) {
 
   }
 
   ngOnInit() {
-    this.events = this.eventService.getEvents();
+    this.events = this.route.snapshot.data['events'];
   }
 
   handleThumbnailClick(eventName) {
